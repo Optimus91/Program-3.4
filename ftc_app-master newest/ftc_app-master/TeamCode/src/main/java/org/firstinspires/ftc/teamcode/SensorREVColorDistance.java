0c1c83fb8930fdd7d851058286dd5c9d3d801793
nickdrones <nickbelbas@earthlink.net>
2017-10-13 16:54:03 -0500

    GOT PIXYCAM WORKING!!!!!!!!!

--- ftc_app-master newest/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorREVColorDistance.java
+++ ftc_app-master newest/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorREVColorDistance.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import android.app.Activity;
@@ -37,6 +8,7 @@ import android.view.View;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -80,13 +52,16 @@ public class SensorREVColorDistance extends LinearOpMode {
      */
     ColorSensor sensorColor;
     DigitalChannel touch;
+    AnalogInput camera;
+    double position =0.0;
 
     @Override
     public void runOpMode() {
 
         // get a reference to the color sensor.
-        sensorColor = hardwareMap.get(ColorSensor.class, "color");
-        touch=hardwareMap.get(DigitalChannel.class,"touch");
+        //sensorColor = hardwareMap.get(ColorSensor.class, "color");
+        //touch=hardwareMap.get(DigitalChannel.class,"touch");
+        camera=hardwareMap.get(AnalogInput.class,"camera");
 
         // hsvValues is an array that will hold the hue, saturation, and value information.
         float hsvValues[] = {0F, 0F, 0F};
@@ -112,18 +87,20 @@ public class SensorREVColorDistance extends LinearOpMode {
             // convert the RGB values to HSV values.
             // multiply by the SCALE_FACTOR.
             // then cast it back to int (SCALE_FACTOR is a double)
-            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
-                    (int) (sensorColor.green() * SCALE_FACTOR),
-                    (int) (sensorColor.blue() * SCALE_FACTOR),
-                    hsvValues);
+           // Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+           //         (int) (sensorColor.green() * SCALE_FACTOR),
+           //         (int) (sensorColor.blue() * SCALE_FACTOR),
+           //         hsvValues);
 
             // send the info back to driver station using telemetry function.
-            telemetry.addData("Alpha", sensorColor.alpha());
-            telemetry.addData("Red  ", sensorColor.red());
-            telemetry.addData("Green", sensorColor.green());
-            telemetry.addData("Blue ", sensorColor.blue());
-            telemetry.addData("Hue", hsvValues[0]);
-            telemetry.addData("Touch: ", !touch.getState());
+            //telemetry.addData("Alpha", sensorColor.alpha());
+            //telemetry.addData("Red  ", sensorColor.red());
+            //telemetry.addData("Green", sensorColor.green());
+            //telemetry.addData("Blue ", sensorColor.blue());
+            //telemetry.addData("Hue", hsvValues[0]);
+            //telemetry.addData("Touch: ", !touch.getState());
+            position = camera.getVoltage();
+            telemetry.addData("Red Position: ", position);
 
             // change the background color to match the color detected by the RGB sensor.
             // pass a reference to the hue, saturation, and value array as an argument
