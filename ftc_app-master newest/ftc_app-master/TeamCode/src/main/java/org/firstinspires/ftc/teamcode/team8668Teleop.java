33df0c69b47184d518f0a14dd9aa402dea760faf
iamlordvoldemort <32399872+iamlordvoldemort@users.noreply.github.com>
2018-01-26 12:16:01 -0600

    Javadocking the Teleop code

--- ftc_app-master newest/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team8668Teleop.java
+++ ftc_app-master newest/ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team8668Teleop.java
@@ -13,35 +13,55 @@ import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODE
 
 @TeleOp(name="8668 Teleop meccanum", group="Teleop")
 
+/**
+ * team8668Teleop extends the <code>OpMode</code> class.
+ *
+ * Contains all of the button and joy-stick initializations for moving the robot with the
+ * remotes as well as the motor and servo initializations for Teleop.
+ *
+ * @author Team 8668
+ * @see OpMode
+ */
 public class team8668Teleop extends OpMode {
-    ////////////////////////////////////////////
-    // This is the Teleop program for driver control.
-    ////////////////////////////////////////////////
+    
+    /** The front right drive motor on the robot. */
     DcMotor rightFront;
+    /** The front left drive motor on the robot. */
     DcMotor leftFront;
+    /** The right rear drive motor on the robot. */
     DcMotor rightRear;
+    /** The left rear drive motor on the robot. */
     DcMotor leftRear;
+    /** The left glyph intake motor -- used in conjunction with the rightGlyph motor to suck in glyphs. */
     DcMotor leftGlyph;
+    /** The right glyph intake motor -- used in conjunction with the leftGlyph motor to suck in glyphs. */
     DcMotor rightGlyph;
-
+   
+    /** The arm servo raises and lowers the jewel arm. */
     Servo arm;
-    Servo glyph;
+    /** The swivel servo swings the jewel sword back and forth to knock off the glyph. */
+    Servo swivel;
+    /** The shoulder servo is a high-torque servo that extends the relic arm up and over the field wall. */
     Servo shoulder;
-    Servo hand;
+    /** The elbow servo raises and lowers the relic for extra flexibility when deploying the relic. */
     Servo elbow;
-    Servo pivot;
-    float launchspeed1;
-    double powerval;
-    double rightVal=0;
-    double leftVal=0;
-    double incrementDir=0;
+    /** The hand servo controls the claw that grabs the relic. */
+    Servo hand;
+
+    /** Setting the start position of the elbow servo. */
     double elbowPos=1;
+    /** Setting the start position of the shoulder servo. */
     double shoulderPos=0.95;
-    double pivotPos =0.522;
+    /** Setting the start position for the hand servo. */
     double handPos=0.7;
+    /** Setting the start position of the swivel servo. */
+    double swivelPos =0.522;
+;
 
     public team8668Teleop() {
     }
+
+    /** Setting the modes and names for all the motors and servos. */
     @Override
     public void init() {
         telemetry.addData ("0", "I AM HERE");
@@ -62,8 +82,7 @@ public class team8668Teleop extends OpMode {
         shoulder = hardwareMap.servo.get("shoulder");
         elbow = hardwareMap.servo.get("elbow");
         hand = hardwareMap.servo.get("hand");
-        glyph = hardwareMap.servo.get("glyph");
-        pivot=hardwareMap.servo.get("jewelSwivel");
+        swivel=hardwareMap.servo.get("jewelSwivel");
         leftGlyph = hardwareMap.dcMotor.get("leftGlyph");
         rightGlyph = hardwareMap.dcMotor.get("rightGlyph");
         leftGlyph.setMode(RUN_USING_ENCODER);
@@ -71,10 +90,11 @@ public class team8668Teleop extends OpMode {
         leftGlyph.setDirection(DcMotor.Direction.REVERSE);
         rightGlyph.setDirection(DcMotor.Direction.FORWARD);
         hand.setPosition(0.7);
-        glyph.setPosition(0.25);
         arm.setPosition(0.0);
-        pivot.setPosition(0.5);
+        swivel.setPosition(0.5);
     }
+
+    /** Reading the raw input from the controllers and turning them into movement values for the motors and servos. */
     @Override
     public void loop() {
         //////////////////////////////////////////
@@ -138,12 +158,12 @@ public class team8668Teleop extends OpMode {
         }
 
         if(gamepad1.x){
-            pivotPos=0.40;
+            swivelPos=0.40;
         }
         else if(gamepad1.b){
-            pivotPos=0.64;
+            swivelPos=0.64;
         }
-        else {pivotPos=0.52;}
+        else {swivelPos=0.52;}
         Range.clip(handPos, 0.35, 0.8);
         rightFront.setPower(RF);
         leftFront.setPower(LF);
@@ -151,7 +171,7 @@ public class team8668Teleop extends OpMode {
         leftRear.setPower(LR);
         elbow.setPosition(elbowPos);
         shoulder.setPosition(shoulderPos);
-        pivot.setPosition(pivotPos);
+        swivel.setPosition(swivelPos);
         hand.setPosition(handPos);
         rightGlyph.setPower(gamepad1.right_trigger-gamepad1.left_trigger);
         leftGlyph.setPower(gamepad1.right_trigger-gamepad1.left_trigger);
@@ -159,14 +179,20 @@ public class team8668Teleop extends OpMode {
       telemetry.addData("shoulder: ",shoulder.getPosition());
       telemetry.addData("grabber:",hand.getPosition());
       telemetry.addData("elbow: ",elbow.getPosition());
-      telemetry.addData("pusher: ",glyph.getPosition());
-      telemetry.addData("pivot: ",pivot.getPosition());
+      telemetry.addData("swivel: ",swivel.getPosition());
       telemetry.addData("hand: ",handPos);
 
     }
+
+    /** Stop everything */
     @Override
     public void stop() {
     }
+
+    /**
+     * Takes the raw data input from the joystick and returns it as a curve to help with smoother robot movement.
+     *
+     * @param dVal The value of the d stick recorded as a double. */
     double scaleInput(double dVal) {
         double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
